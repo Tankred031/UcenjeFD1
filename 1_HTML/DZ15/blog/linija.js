e0d01c40fad337384af6ece64fb1edacfd3a2849
Tankred031 <tankred.kralj@gmail.com>
2026-01-17 14:30:56 +0100

    blog gotov

---
+++ 1_HTML/DZ15/blog/linija.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const scrollPercent = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width =
+        scrollPercent + "%";
+});
+
+console.log("JS is working");
