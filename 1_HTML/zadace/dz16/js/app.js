13da13363ff01178aad746f92d9e5e15c3850209
Tankred031 <tankred.kralj@gmail.com>
2026-01-26 13:18:47 +0100

    dz16

--- 1_HTML/zadace/dz16/js/app.js
+++ 1_HTML/zadace/dz16/js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+$(function () {
+  var $menu = $('#desktop-menu');
+
+  $('.menu-icon').on('click', function () {
+    if (window.matchMedia('(max-width: 639px)').matches) {
+      $menu.toggleClass('is-open');
+    }
+  });
+});
+
