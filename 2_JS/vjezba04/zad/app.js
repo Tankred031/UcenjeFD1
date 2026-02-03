9fea4a4cdf40723bf779e8e9f5c3d4df7c951543
Tankred031 <tankred.kralj@gmail.com>
2026-02-03 19:53:17 +0100

    Update app.js
    Nadopunjeno sve s predavanja

--- 2_JS/vjezba04/zad/app.js
+++ 2_JS/vjezba04/zad/app.js
@@ -1,81 +1,14 @@
 
 const rezultat = document.getElementById('rezultat');
-document.getElementById('izvedi').addEventListener('click', () => {rezultat.innerHTML='';
-
+document.getElementById('izvedi').addEventListener('click', () => {
+  rezultat.innerHTML='';
   const a = document.getElementById('a').value;
   const b = document.getElementById('b').value;
   const c = document.getElementById('c').value;
   const d = document.getElementById('d').value;
   const zadatak = document.getElementById('zadatak').value;
 
-  if(zadatak==='2'){
-  
-  const x = Number(a);  
-  if(!x){
-    rezultat.innerHTML='A nije broj';
-    return
-  }
-  const y = Number(b);  
-  if(!y){
-    rezultat.innerHTML='B nije broj';
-    return
-  }
-  const z = Number(c);  
-  if(!z){
-    rezultat.innerHTML='C nije broj';
-    return
-  }
-  if(x<y&&y<z){
-    rezultat.innerHTML=x
-  }else if(y<x&&y<z){
-    rezultat.innerHTML=y
-  }else{
-    rezultat.innerHTML=z
-  }
-
-  return
-  }
-
-
-  if(zadatak==='4'){
-    //ovdje rješavati 
-  }
-
-
-
-  
-  if (zadatak === '3'){
-    const x = Number(a)
-    const y = Number(b)
-    const z = Number(c)
-    const q = Number(d)
-
-    if(isNaN(x)) {
-      rezultat.innerHTML='A nije broj'
-      return
-    }
-    if(isNaN(y)) {
-      rezultat.innerHTML='B nije broj'
-      return
-    }
-    if(isNaN(z)) {
-      rezultat.innerHTML='C nije broj'
-      return
-    }
-    if(isNaN(q)) {
-      rezultat.innerHTML='D nije broj'
-      return
-    }
-
-    rezultat.innerHTML=x+y+z+q
-
-    //rezultat.innerHTML=a+b+c+d  spaja stringove
-
-
-    return
-  }
-  
-  // Za svaki zadatak zahtijevamo određene ulaze
+// Za svaki zadatak zahtijevamo određene ulaze
   if (zadatak === '1') {
     //console.log('1. zadatak');
     //console.log(a);
@@ -102,6 +35,8 @@ document.getElementById('izvedi').addEventListener('click', () => {rezultat.inne
     rezultat.innerHTML='A i B su isti'
   }
     
+  // ovo smo imali prije x i y 
+
   /*if(a>b){
     rezultat.innerHTML=a;
   }else{
@@ -111,13 +46,102 @@ document.getElementById('izvedi').addEventListener('click', () => {rezultat.inne
   
   return; // short curcuiting prekida izvođenje cijele fukcije ()=>{}
   }
-
-  // Ovdje će doći drugi zadatak
-
   
   // ovo će se ispisati ako u HTML dodatke option za zadatak a ovdje ga ne obradite
   rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
-});
+  });
 
 
+  
+  if(zadatak==='2'){
+  
+  const x = Number(a);  
+  if(!x){
+    rezultat.innerHTML='A nije broj';
+    return
+  }
+  const y = Number(b);  
+  if(!y){
+    rezultat.innerHTML='B nije broj';
+    return
+  }
+  const z = Number(c);  
+  if(!z){
+    rezultat.innerHTML='C nije broj';
+    return
+  }
+  if(x<y&&x<z){
+    rezultat.innerHTML=x
+  }else if(y<x&&y<z){
+    rezultat.innerHTML=y
+  }else{
+    rezultat.innerHTML=z
+  }
+  return
+  }
 
+
+    
+  if (zadatak === '3'){
+    const x = Number(a)
+    const y = Number(b)
+    const z = Number(c)
+    const q = Number(d)
+
+    if(isNaN(x)) {
+      rezultat.innerHTML='A nije broj'
+      return
+    }
+    if(isNaN(y)) {
+      rezultat.innerHTML='B nije broj'
+      return
+    }
+    if(isNaN(z)) {
+      rezultat.innerHTML='C nije broj'
+      return
+    }
+    if(isNaN(q)) {
+      rezultat.innerHTML='D nije broj'
+      return
+    }
+
+    rezultat.innerHTML=x+y+z+q
+    //rezultat.innerHTML=a+b+c+d  spaja stringove
+
+    return
+  }
+  
+
+if(zadatak==='4'){
+    // ovdje rješavati 4. zadatak
+
+  //  Provjera punoljetnosti: 
+  //  Korisnik unosi godinu rođenja u polje A. 
+  //  Izračunaj njegovu dob u odnosu na tekuću godinu i ispiši je li punoljetan ili nije.
+
+
+  /*
+  [ZADATAK 5]
+ Pozitivan, negativan ili nula: 
+ Za uneseni broj u polju A ispiši poruku je li on pozitivan, negativan ili je nula.
+
+ ----------------------------------------------------------------
+ 
+ [ZADATAK 6]
+ Parnost broja: 
+ Provjeri je li uneseni broj u polju A paran ili neparan koristeći modulo operator (%).
+
+ ----------------------------------------------------------------
+ 
+ [ZADATAK 7]
+ Ocjenjivanje: 
+ Na temelju unesenog broja bodova (0-100) u polje A, ispiši ocjenu:
+ 90-100: Odličan, 80-89: Vrlo dobar, 70-79: Dobar, 60-69: Dovoljan, <60: Nedovoljan.
+
+ ----------------------------------------------------------------
+ 
+ [ZADATAK 8]
+ Djeljivost: 
+ Provjeri je li broj u polju A djeljiv s brojem u polju B bez ostatka. 
+ Ispiši "Djeljiv je" ili "Nije djeljiv".  
+  */
