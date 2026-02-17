2957e4c873a9c01eab8be5ff134d09ce146bb95a
Tankred031 <tankred.kralj@gmail.com>
2026-02-17 18:05:39 +0100

    while petlja sat 1

---
+++ 2_JS/vjezba08/script.js
@@ -0,0 +1,56 @@
+
+// beskonačna while petlja -- mora imati -- break
+while(true){
+    console.log('Edunova');
+    break;
+}
+
+let brojac = 0;
+console.time('while petlja');
+while(brojac++ < 100){
+    //console.log(brojac);
+}
+console.timeEnd('while petlja');
+
+
+let brojUnos = '1262';
+// zbroji sve znamenke danog broja
+// ulaz: 1262, izlaz: 11
+
+console.time('ZB1');
+let zbroj=0;
+for (let i=0; i<brojUnos.length; i++){
+    zbroj += parseInt(brojUnos[i]);
+}
+console.log(zbroj);
+console.timeEnd('ZB1');
+
+console.time('ZB2');
+let broj = parseInt(brojUnos);
+zbroj = 0;
+while(broj>0){
+    zbroj += broj % 10;
+    broj = broj - (broj % 10);
+    broj = broj / 10;
+}
+console.log(zbroj);
+console.timeEnd('ZB2');
+
+// čitati više o optimizaciji
+// big o notacija
+// smanjiti kompleksnost je riješenje optimizacije
+
+// ČINJENICA: niti u for, niti u while se ne mora ući
+
+let br = -5; // ova vrijednost je ostalo od koda prije
+
+for(let i=0; i>br; i--){
+    console.log('Ušao u for pelju', i);    
+}
+
+// dođu podaci s API
+let podaciSAPI = [];  // [{ime: 'Pero'}, {ime: 'Marko'}];
+
+while(podaciSAPI.length>0){
+    console.log('Ušao u while petlju', podaciSAPI.pop()?.ime);    
+}
