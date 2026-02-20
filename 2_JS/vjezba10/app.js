17211bf1eedc4a4bf7e5455b6c2d0cd22cbd078e
Tankred031 <tankred.kralj@gmail.com>
2026-02-20 18:50:48 +0100

    rekurzija

---
+++ 2_JS/vjezba10/app.js
@@ -0,0 +1,23 @@
+
+// rekurzija - kada funkcija zove samu same uz uvjet prekida rekurzije
+
+let sum = 0;
+for(let i=1; i<=100; i++){
+    sum += i;
+}
+console.log(sum);
+
+function zbroji(broj){
+    if(broj === 1){
+        return 1;
+    }
+    return broj + zbroji(broj-1);
+}
+console.log(zbroji(100));
+
+// stackoverflow
+
+function so(){
+    so ();
+}
+console.log();
