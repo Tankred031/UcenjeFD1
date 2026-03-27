e456fa07aba6cff2a1da05371f25802fe6ef6430
Tankred031 <tankred.kralj@gmail.com>
2026-03-27 18:48:28 +0100

    lottie

--- 3_REACT/EdunovaAPP/src/pages/Home.jsx
+++ 3_REACT/EdunovaAPP/src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { IME_APLIKACIJE } from "../constants";
 
 export default function Home() {
     return (
         <>
             <h1>Dobrodošli na {IME_APLIKACIJE}</h1>
-            <div>
-                Ovdje dođe slika
+            <div style={{maxWidth: '300px', margin: 'auto'}}>
+                <DotLottieReact
+                src="/bottle.lottie"
+
+                loop
+                autoplay
+
+                />
             </div>
         </>
     )
